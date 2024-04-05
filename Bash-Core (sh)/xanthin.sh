f2d5f093e8149494edd73a828f1b145d9664e472
Poya <158841443+Poyamo@users.noreply.github.com>
2024-04-05 19:54:39 +0330

    Update xanthin.sh

--- Bash-Core (sh)/xanthin.sh
+++ Bash-Core (sh)/xanthin.sh
@@ -1,1 +1,7 @@
-h
+#!bin/bash
+function xanthinRun(){
+  echo "© xanthin 2024. Type Your Code From under line."
+  echo "plases type your xan file address"
+  $ python /storage/emulated/0/Xanthin/Bash/Xanthin_loader.py
+  }
+  
