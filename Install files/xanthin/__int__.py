63f7a501b9a6da61e745b87524a8a6df4d6fbc21
Poya <158841443+Poyamo@users.noreply.github.com>
2024-04-05 09:15:34 +0330

    Create __int__.py

---
+++ Install files/xanthin/__int__.py
@@ -0,0 +1,181 @@
+#this compiler_def for android.
+import os
+import shutil
+import importlib
+class xanthin:
+	def run(fname):
+			empty = open(fname,"r")
+			gaza = empty.read()
+			em = gaza.replace("void main or mains(","\nfrom xanthin import *\n")
+			t = em.replace("-","")
+			y = t.replace(":","=")
+			mes = y.replace("=>",":")
+			i = mes.replace("?","(")
+			samorai = i.replace("!",")")
+			rabet = samorai.replace("$","")
+			act = rabet.replace("action","def")
+			roo = act.replace("not","else")
+			lostin = roo.replace("use","from")
+			way = lostin.replace("add","import")
+			want = way.replace("even","for")
+			tose = want.replace("||","#")
+			clas = tose.replace("@","class")
+			saveme = clas.replace(";","")
+			orlandk = saveme.replace("NAME","__name__")
+			pythlib = orlandk.replace("load python.requests","import requests")
+			fucs = pythlib.replace("load python.socket","import socket")
+			child = fucs.replace("load python.gzip","import gzip")
+			human = child.replace("load python.bz2","import bz2")
+			kkkkk = human.replace("load python.lzma","import lzma")
+			go = kkkkk.replace("\329","!")
+			smart = go.replace("\328","?")
+			mu = smart.replace("\327",";")
+			life = mu.replace("\326","use")
+			fam = life.replace("\325","add")
+			com = fam.replace("\324","@")
+			fact = com.replace("in sub","import subprocess")
+			heart = fact.replace("sub out","subprocess.check_output")
+			faaa = heart.replace("shot","try")
+			rap = faaa.replace("put in place of","replace")
+			ok = rap.replace("reback","return")
+			array = ok.replace("_","[")
+			dead = array.replace("__","]")
+			aee = dead.replace("num","len")
+			pain = aee.replace("++","__")
+			teb = pain.replace("riot","exec")
+			sad = teb.replace("string ","")
+			blueeyes = sad.replace("inte ","")
+			redeyes = blueeyes.replace("arr ","")
+			xcxxx = redeyes.replace(" AND ","_")
+			padrmoni = xcxxx.replace(" DOT ",".")
+			akf = padrmoni.replace("true","True")
+			zanb = akf.replace("false","False")
+			lala = zanb.replace("TRUE","true")
+			gokan = lala.replace("END","#script_end")
+			ala = gokan.replace("more else","elif")
+			skola = ala.replace("unaction","lambda")
+			exec(ala)
+
+import os
+import os.path
+import shutil
+import importlib
+import subprocess
+import json
+import sys
+from varname.helpers import Wrapper
+from varname import varname
+#info of xanthin package.
+version_of_xanthin = "0.0.1"
+build = "DHJ-185-0823-mar"
+class message:
+	def show(a):
+		print(a)
+    
+to = "to"
+write = "write"
+local = "local"
+email = "email"
+class var:
+   def set(g , to ,numb):
+   	globals()[g] = numb
+
+class file:
+   def create(name, write , s):
+   	f = open(name,"a")
+   	f.write(s)
+   	f.close()
+   
+   def read(name, local , mkm):
+   	k = open(mkm,"r")
+   	k.read()
+   	
+   def copy(name , to ,sname):
+      shutil.copy(name, sname)
+      
+   def delete(name):
+   	os.remove(name)
+
+   def be(fname):
+   	os.path.exists(fname)
+
+
+   def delete__folder__(fname):
+   		os.rmdir(fname)
+  	
+   def move(fname , to ,sdf):
+   	shutil.move(fname,sdf)
+
+
+
+against = "against"
+def Acceptation(fname , against ,lets):
+    x = __import__(fname)
+    
+class archive:
+	def create(file_type, local , to ,ziplocal):
+		shutil.make_archive(local, file_type ,zipelocal)
+		
+
+run = "run"	
+void = "void"
+
+def type(hjk,xcv):
+	    if(xcv==""):
+	       hjk
+	    else:
+	    	hjk
+	    	xcv
+	    	class top:
+	    		print("")
+	    		
+
+def call(fname,x):
+	if(x=="true"):
+		lo = subprocess.call(fname, shell=x)
+		lo
+	else:
+		if(x=="false"):
+			lo = subprocess.call(fname, shell=x)
+			lo
+		else:
+			print("error from Xanthin")
+			print("error: you can type only boolen's.")
+	
+def main(fname):
+	if(fname==""):
+		print("ERROR FROM XANTHIN")
+		print("error: main is empty.")
+	else:
+		fname
+
+import sys 
+
+
+import time
+def delete_cahac():
+	file.delete(b+".py")
+
+class inputs:
+	def new():
+		input()
+
+def mgl():
+	return varname()
+
+def show():
+	return varname()
+	
+class center:
+	def bash(fname):
+		k = open(fname, "r")
+		see = k.read()
+		mgs = see.replace("echo","print(")
+		carden = mgs.replace("' ",")")
+		exec(carden)
+
+#from xanthin_compiler_defs import *
+import os
+import time
+b = input()
+xanthin.run(b)
